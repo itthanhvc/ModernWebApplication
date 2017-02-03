cdaea5d0b1da845a4321edca6edaaae2b10e47c5
itthanhvc <chienxa_1986@yahoo.com.vn>
2017-02-03 11:54:20 -0600

    updated

--- ServiceExercise/src/app/error.component.ts
+++ ServiceExercise/src/app/error.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<p>
+  template: `<h2> Error</h2>
+  <p>
       Cannot find the student profile
     </p> `
 })
--- ServiceExercise/src/app/home.component.ts
+++ ServiceExercise/src/app/home.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<p>
-      <strong> Maharishi University Of Management</strong><br/>
+  template: `<h2> Maharishi University Of Management</h2>
+  <p>
       We come from many places, cultures, and backgrounds, but share a strong common commitment to personal inner growth, wellness, sustainability, and positive values.<br/>
       Many of us have experienced the stress and negativity so common at other colleges. That's why we came to MUM!<br/>
       We support a progressive and inclusive campus culture that’s creative, dynamic, and focused on making the world a better, more peaceful place.<br/>
